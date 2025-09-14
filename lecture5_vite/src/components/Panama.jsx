1d00f7b67b8e4db111e22fa90a5c882f2d8efd46
zawarali0302-commits <zawarali0302@gmail.com>
2025-09-14 01:11:06 -0700

    panama flag

--- lecture5_vite/src/components/Panama.jsx
+++ lecture5_vite/src/components/Panama.jsx
@@ -1,16 +1,17 @@
 import React from 'react'
 
 const Panama = () => {
-  return (
-    <div>
-      <h1>Panama</h1>
-      <div className='border w-72 h-36 flex'>
-        <div className='bg-blue-700 w-1/2'></div>
-        <div className='bg-red-500 w-1/2 h-1/2'></div>
-      </div>
-      
-    </div>
-  )
+    return (
+        <div>
+            <h1>Panama</h1>
+            <div className='border w-72 h-36 flex'>
+                <div className='bg-blue-700 w-1/2'>
+                <div className='bg-white h-1/2'></div>
+                </div>
+                <div className='bg-red-500 w-1/2 h-1/2'></div>
+            </div>
+        </div>
+    )
 }
 
 export default Panama
